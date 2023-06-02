7439c1ce5c45d896cf098ec2372109c6380addf3
Dmytro Stadnytskyi <dima092339@gmail.com>
2023-06-02 23:31:36 +0200

    Refactor: Replace @ZeebeDeployment with @Deployment
    @ZeebeDeployment is deprecated

--- src/main/java/com/avg/kreditantrag/kreditantrag/KreditantragApplication.java
+++ src/main/java/com/avg/kreditantrag/kreditantrag/KreditantragApplication.java
@@ -1,13 +1,13 @@
 package com.avg.kreditantrag.kreditantrag;
 
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
-import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
+import io.camunda.zeebe.spring.client.annotation.Deployment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableZeebeClient
-@ZeebeDeployment(resources = "classpath*:/model/*.bpmn")
+@Deployment(resources = "classpath*:/model/*.bpmn")
 public class KreditantragApplication {
 
     public static void main(String... args) {
