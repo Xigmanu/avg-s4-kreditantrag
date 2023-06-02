d4639d8152d2ce87b30dff64bfe67263e1cc9a87
Dmytro Stadnytskyi <dima092339@gmail.com>
2023-06-02 23:28:49 +0200

    Refactor: Remove unused imports

--- src/main/java/com/avg/kreditantrag/kreditantrag/KreditantragApplication.java
+++ src/main/java/com/avg/kreditantrag/kreditantrag/KreditantragApplication.java
@@ -1,10 +1,7 @@
 package com.avg.kreditantrag.kreditantrag;
 
-import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.ZeebeDeployment;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
