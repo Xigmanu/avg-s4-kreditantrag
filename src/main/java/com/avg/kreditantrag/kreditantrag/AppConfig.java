be4473e39f88df1136a2acdd58290da265135a18
Dmytro Stadnytskyi <dima092339@gmail.com>
2023-06-03 12:05:53 +0200

    Change configuration
    Add beans for profiles `cloud` and `self-managed`.
    Required properties are no longer hard-coded, but are read directly from the properties file.

--- src/main/java/com/avg/kreditantrag/kreditantrag/AppConfig.java
+++ src/main/java/com/avg/kreditantrag/kreditantrag/AppConfig.java
@@ -2,15 +2,41 @@ package com.avg.kreditantrag.kreditantrag;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.ZeebeClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class AppConfig {
+    public static final String JOB_TYPE = "kreditantrag";
+
+    @Value("${zeebe.client.cloud.clusterId}")
+    private String clusterId;
+
+    @Value("${zeebe.client.cloud.clientId}")
+    private String clientId;
+
+    @Value("${zeebe.client.cloud.clientSecret}")
+    private String clientSecret;
+
+    @Value("${zeebe.client.broker.gateway-address}")
+    private String gatewayAddress;
+
     @Bean
-    public ZeebeClientBuilder zeebeClient() {
+    @Profile("cloud")
+    public ZeebeClientBuilder zeebeClientBuilderCloud() {
+        return ZeebeClient.newCloudClientBuilder()
+                .withClusterId(clusterId)
+                .withClientId(clientId)
+                .withClientSecret(clientSecret)
+                .withRegion("dsm-1");
+    }
+    @Bean
+    @Profile("self-managed")
+    public ZeebeClientBuilder zeebeClientBuilderLocal() {
         return ZeebeClient.newClientBuilder()
-                .gatewayAddress("127.0.0.1:26500")
+                .gatewayAddress(gatewayAddress)
                 .usePlaintext();
     }
 }
