3b5d4f4d4462718de2c07e54c52f913a4471e594
Dmytro Stadnytskyi <dima092339@gmail.com>
2023-06-04 14:54:28 +0200

    Apply the `UrlBuilder` to build URLs for requests

--- src/main/java/com/avg/kreditantrag/internal/service/KreditService.java
+++ src/main/java/com/avg/kreditantrag/internal/service/KreditService.java
@@ -1,17 +1,30 @@
 package com.avg.kreditantrag.internal.service;
 
+import com.avg.kreditantrag.KreditantragApplication;
+import com.avg.kreditantrag.internal.helper.UrlBuilder;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-import static com.avg.kreditantrag.internal.helper.UrlHelper.getUrl;
-
 public interface KreditService {
-    default Response getGetResponse(String url, OkHttpClient httpClient) throws IOException {
+    default Response getGetResponse(String endpoint,
+                                    Object pathVariable,
+                                    String queryParameter,
+                                    Object queryParameterValue,
+                                    OkHttpClient httpClient) throws IOException {
+        UrlBuilder builder = new UrlBuilder();
+        String url = builder
+                .endpoint(endpoint)
+                .pathVariable(pathVariable)
+                .queryParameter(queryParameter, queryParameterValue)
+                .build();
+
         Request request = new Request.Builder()
                 .url(url)
                 .build();
@@ -21,7 +34,8 @@ public interface KreditService {
     default Response getPostResponse(String endpoint,
                                  String json,
                                  OkHttpClient httpClient) throws IOException {
-        String url = getUrl(endpoint, null);
+        UrlBuilder builder = new UrlBuilder();
+        String url = builder.endpoint(endpoint).build();
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody requestBody = RequestBody.create(mediaType, json);
 
