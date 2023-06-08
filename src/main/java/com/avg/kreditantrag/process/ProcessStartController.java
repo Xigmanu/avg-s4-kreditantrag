6aa887ccc0c569170b73cfcd518812b7bf280074
Dmytro Stadnytskyi <dima092339@gmail.com>
2023-06-09 00:25:39 +0200

    Add initialization for `system-intern` process

--- src/main/java/com/avg/kreditantrag/process/ProcessStartController.java
+++ src/main/java/com/avg/kreditantrag/process/ProcessStartController.java
@@ -11,14 +11,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/")
 public class ProcessStartController {
     private static final Logger LOGGER = LoggerFactory.getLogger(KreditantragApplication.class);
     private final ZeebeClient client;
@@ -28,18 +29,27 @@ public class ProcessStartController {
         this.client = client;
     }
 
-    @RequestMapping(value = "/start", method = RequestMethod.GET)
+    @PostMapping("/start")
     public void startKreditantragProcess(@RequestBody Map<String, Object> variables) {
         LOGGER.info("Starting process");
 
         ProcessInstanceEvent processInstanceEvent = client
                 .newCreateInstanceCommand()
-                .bpmnProcessId("employee-test")
+                .bpmnProcessId("employee-process")
                 .latestVersion()
                 .variables(variables)
                 .send()
                 .join();
 
-        LOGGER.info("Started: processInstanceKey={}", processInstanceEvent.getProcessInstanceKey());
+        LOGGER.info("Started: processInstanceKey={}, bpmnProcessId={}",
+                processInstanceEvent.getProcessInstanceKey(),
+                processInstanceEvent.getBpmnProcessId());
+
+        client.newCreateInstanceCommand()
+                .bpmnProcessId("system-intern")
+                .latestVersion()
+                .variables(variables)
+                .send()
+                .join();
     }
 }
